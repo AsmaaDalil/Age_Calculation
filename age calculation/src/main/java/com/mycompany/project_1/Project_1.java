e1c2f4bba2d0e590cdcd2437237a0592616f9215
Asmaa Dalil <108135622+AsmaaDalil@users.noreply.github.com>
2024-05-01 18:57:27 +0300

    Add files via upload

---
+++ age calculation/src/main/java/com/mycompany/project_1/Project_1.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.project_1;
+
+/**
+ *
+ * @author ACAD CENTER
+ */
+public class Project_1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
